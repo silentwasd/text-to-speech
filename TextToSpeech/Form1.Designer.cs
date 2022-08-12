6589dae5bf9a81a53a5f68d44a8a2a5be81e17ab
silentwasd <silentwasd@gmail.com>
2022-08-12 19:27:43 +0300

    Добавьте файлы проекта.

---
+++ TextToSpeech/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 18);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(402, 466);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(367, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Озвучить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(425, 80);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(367, 204);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(421, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Доступные слова:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Предлагаю:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(425, 320);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(367, 164);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 499);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ TextToSpeech/Form1.cs
@@ -0,0 +1,148 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        string[] availableWords;
+
+        WaveOutEvent outputDevice;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadVoice();
+        }
+
+        private void LoadVoice()
+        {
+            var files = Directory.GetFiles("./words", "*.wav");
+
+            var words = new List<string>();
+
+            foreach (var file in files)
+            {
+                words.Add(Path.GetFileNameWithoutExtension(file));
+            }
+
+            var directories = Directory.GetDirectories("./words");
+
+            foreach (var directory in directories)
+            {
+                words.Add(Path.GetFileName(directory));
+            }
+
+            words.Sort();
+            availableWords = words.ToArray();
+
+            listBox1.Items.AddRange(availableWords);
+        }
+
+        private ISampleProvider RenderVoice(string[] lines)
+        {
+            var samples = new List<ISampleProvider>();
+            var rand = new Random();
+
+            for (int l = 0; l < lines.Length; l++)
+            {
+                string[] words = lines[l].Split(' ');
+
+                for (int w = 0; w < words.Length; w++)
+                {
+                    string _word = words[w].ToLower();
+
+                    if (String.IsNullOrEmpty(_word))
+                        continue;
+
+                    if (File.Exists("./words/" + _word + ".wav"))
+                    {
+                        samples.Add(new AudioFileReader("./words/" + _word + ".wav"));
+                    }
+                    else if (Directory.Exists("./words/" + _word))
+                    {
+                        var files = Directory.GetFiles("./words/" + _word);
+                        samples.Add(new AudioFileReader(files[rand.Next(0, files.Length)]));
+                    }
+
+                    if (w < words.Length - 1)
+                        samples.Add(new SilenceProvider(samples[0].WaveFormat).ToSampleProvider().Take(TimeSpan.FromMilliseconds(50)));
+                }
+
+                if (l < lines.Length - 1)
+                    samples.Add(new SilenceProvider(samples[0].WaveFormat).ToSampleProvider().Take(TimeSpan.FromMilliseconds(300)));
+            }
+
+            return new ConcatenatingSampleProvider(samples);
+        }
+
+        private void PlayVoice(ISampleProvider provider)
+        {
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+
+            if (outputDevice.PlaybackState == PlaybackState.Playing)
+                outputDevice.Stop();
+
+            outputDevice.Init(provider);
+            outputDevice.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = textBox1.Text;
+
+            string[] lines = text.Split("\r\n".ToCharArray());
+
+            var voice = RenderVoice(lines);
+
+            PlayVoice(voice);
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            outputDevice.Dispose();
+            outputDevice = null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string substring = "";
+
+            int selectionStart = textBox1.SelectionStart;
+            int offset = 1;
+
+            while (selectionStart > 0)
+            {
+                substring = text.Substring(--selectionStart, offset++);
+
+                if (substring.StartsWith(" ") || substring.StartsWith("\n"))
+                {
+                    substring = substring.Substring(1);
+                    break;
+                }
+            }
+
+            listBox2.Items.Clear();
+
+            if (String.IsNullOrEmpty(substring))
+                return;
+
+            listBox2.Items.AddRange(availableWords.Where(word => word.StartsWith(substring.ToLower())).ToArray());
+        }
+    }
+}
